aea45076841e09760db30b682074f49f0fd37ba1
iigyeonghag-source <iigyeonghag@gmail.com>
2026-05-17 11:04:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import discord
 import random
 import asyncio
-from discord.ext import commands
+from discord.ext import commands, tasks
 from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from discord import app_commands
